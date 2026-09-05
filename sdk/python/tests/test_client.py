6b99f299f0955d27808c4fd91a27aca791647c69
dlamaro96 <daniamaro96@gmail.com>
2026-09-05 08:55:00 +0400

    security: apply least privilege to supply chain workflows
    Signed-off-by: dlamaro96 <daniamaro96@gmail.com>

--- sdk/python/tests/test_client.py
+++ sdk/python/tests/test_client.py
@@ -1,7 +1,15 @@
-from inferqos_client import headers, interactive
-def test_scope_restores_headers():
-    with interactive(250) as value:
-        assert value["X-InferQoS-Deadline-Ms"] == "250"
-        assert headers()["X-InferQoS-Class"] == "interactive"
-    assert headers() == {}
+import unittest
 
+from inferqos_client import headers, interactive
+
+
+class ClientTests(unittest.TestCase):
+    def test_scope_restores_headers(self):
+        with interactive(250) as value:
+            self.assertEqual(value["X-InferQoS-Deadline-Ms"], "250")
+            self.assertEqual(headers()["X-InferQoS-Class"], "interactive")
+        self.assertEqual(headers(), {})
+
+
+if __name__ == "__main__":
+    unittest.main()
